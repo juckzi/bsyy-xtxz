20757ac3e5a649e30ca405bdbf5b818262a6d84a
morrisduan <morris.imb@gmail.com>
2018-08-29 08:26:07 +0800

    添加首页模块

--- xtxz/urls.py
+++ xtxz/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('',include('index.urls'),name='index'),
     path('admin/', admin.site.urls),
     path('xxxt/', include('xxxt.urls'),name='xxxt'),
     path('server/',include('server.urls'),name='server')
